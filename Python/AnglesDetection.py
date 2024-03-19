eda3836f4e3fd9aecd90f84e37cdabba8c73ef15
Chang Hui Simone Lin <daregox@gmail.com>
2024-03-19 19:17:15 +0000

    batch data

---
+++ Python/AnglesDetection.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 15:31:01 2024
+
+@author: simone
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+from copy import deepcopy
+
+import os
+import glob
+
+import DataManagement as Data
+
+class Angles:
+    def __init__(self) -> None:
+        pass
